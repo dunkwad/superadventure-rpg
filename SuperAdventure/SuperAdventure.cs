2ccb088b2ee107f686a3afd45f0ee335fd006b5c
dunkwad <dunkwad@gmail.com>
2025-04-22 22:47:30 +0800

    Add files up to Lesson 10

---
+++ SuperAdventure/SuperAdventure.cs
@@ -0,0 +1,21 @@
+using Engine;
+
+namespace SuperAdventure
+{
+    public partial class SuperAdventure : Form
+    {
+        private Player _player = new Player(10, 10, 20, 0, 1);
+
+        public SuperAdventure()
+        {
+            InitializeComponent();
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
+            Location location = new Location(1, "Home", "This is your house.");
+        }
+    }
+}
