d68ad2d42f9a53256a41f87e2977da29ffb09bed
dunkwad <dunkwad@gmail.com>
2025-05-13 17:04:07 +0800

    Add Player functions

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -21,8 +21,110 @@ namespace Engine
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
+        }
+
+        public bool HasRequiredItemToEnterThisLocation(Location location)
+        {
+            if (location.ItemRequiredToEnter == null) return true;
+
+            foreach(InventoryItem ii in Inventory)
+            {
+                if (ii.Details.ID == location.ItemRequiredToEnter.ID) return true;
+            }
+
+            return false;
+        }
+
+        public bool HasThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == quest.ID) return true;
+            }
+
+            return false;
+        }
+
+        public bool CompletedThisQuest(Quest quest)
+        {
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                if (playerQuest.Details.ID == quest.ID) return playerQuest.IsCompleted;
+            }
+
+            return false;
+        }
+
+        public bool HasAllQuestCompletionItems(Quest quest)
+        {
+            // Check if players have all items needed to complete the quest here
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                bool foundItemInPlayersInventory = false;
+
+                // Check each item in player's inventory to see if they have it and enough of it
+                foreach (InventoryItem ii in Inventory)
+                {
+                    if (ii.Details.ID == qci.Details.ID) // Player has item in their inventory
+                    {
+                        foundItemInPlayersInventory = true;
+
+                        if (ii.Quantity < qci.Quantity) return false; //Player does not have enough of item in their inventory to complete the quest
+                    }
+                }
+
+                // Player does not have the quest completion item in their inventory
+                if (!foundItemInPlayersInventory) return false;
+            }
+
+            // If function reached this point, then player must have all the required items and enough of them to complete the quest
+            return true;
+        }
+
+        public void RemoveQuestCompletionItems(Quest quest)
+        {
+            foreach(QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                foreach(InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID)
+                    {
+                        ii.Quantity -= qci.Quantity;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void AddItemToInventory(Item itemToAdd)
+        {
+            foreach(InventoryItem ii in Inventory)
+            {
+                // If player already has item in their inventory, increase quantity by one
+                if(ii.Details.ID == itemToAdd.ID)
+                {
+                    ii.Quantity++;
+                    return;
+                }
+            }
+
+            // If player doesn't have item in their inventory, add it with a quantity of 1
+            Inventory.Add(new InventoryItem(itemToAdd, 1));
+        }
+
+        public void MarkQuestCompleted(Quest quest)
+        {
+            foreach(PlayerQuest pq in Quests)
+            {
+                if(pq.Details.ID == quest.ID)
+                {
+                    pq.IsCompleted = true;
+                    return;
+                }
+            }
         }
     }
 }
